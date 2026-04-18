de2baf39fd0857f4541f3e51047cc0edc693e820
고도영 <kgracy@naver.com>
2026-04-18 16:01:25 +0900

    feat: 곱셈(multiply) 함수 구현 및 테스트 (#9)

---
+++ multiply.py
@@ -0,0 +1,8 @@
+# multiply.py
+# 곱셈 연산 계산기 모듈 | 작성자: D0Zer0009 | 2026-04-18
+
+def multiply(a, b):
+    """두 수의 곱을 반환합니다.
+    Args: a, b (float): 곱할 두 수
+    Returns: float: a * b"""
+    return a * b
